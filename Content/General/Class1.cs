8a19bf7e490933617136e8ca11e3b8be751c56c3
ORION <n/a>
2024-07-24 07:05:16 -0700

    Music stuff, Needs bug fixes.

--- Content/General/Class1.cs
+++ Content/General/Class1.cs
@@ -25,7 +25,46 @@ namespace EventHorizons.Content.General
             return Main.npc.Any(n => n.active && n.type == NPCID.KingSlime);
         }
 
-        public override int Music => MusicLoader.GetMusicSlot(Mod, "Content/Assets/Music/Boss/KStheme"); //make sure the music file is in a Music folder!
+        public override int Music => MusicLoader.GetMusicSlot(Mod, "Content/Assets/Music/Boss/King_Slime"); //make sure the music file is in a Music folder!
+    }
+    internal class EOCMusic : ModSceneEffect
+    {
+        public override SceneEffectPriority Priority => SceneEffectPriority.BossHigh;
+        public override bool IsSceneEffectActive(Player player)
+        {
+            return Main.npc.Any(n => n.active && n.type == NPCID.EyeofCthulhu);
+        }
+
+        public override int Music => MusicLoader.GetMusicSlot(Mod, "Content/Assets/Music/Boss/Eye_Of_Cthulhu");
+    }
+    internal class QBMusic : ModSceneEffect
+    {
+        public override SceneEffectPriority Priority => SceneEffectPriority.BossHigh;
+        public override bool IsSceneEffectActive(Player player)
+        {
+            return Main.npc.Any(n => n.active && n.type == NPCID.QueenBee);
+        }
+
+        public override int Music => MusicLoader.GetMusicSlot(Mod, "Content/Assets/Music/Boss/A_Bee-g_Trouble");
+    }
+    internal class SkeleMusic : ModSceneEffect
+    {
+        public override SceneEffectPriority Priority => SceneEffectPriority.BossHigh;
+        public override bool IsSceneEffectActive(Player player)
+        {
+            return Main.npc.Any(n => n.active && n.type == NPCID.SkeletronHead);
+        }
+
+        public override int Music => MusicLoader.GetMusicSlot(Mod, "Content/Assets/Music/Boss/Skeletron");
+    }
+    internal class WOFMusic : ModSceneEffect
+    {
+        public override SceneEffectPriority Priority => SceneEffectPriority.BossHigh;
+        public override bool IsSceneEffectActive(Player player)
+        {
+            return Main.npc.Any(n => n.active && n.type == NPCID.WallofFlesh);
+        }
+
+        public override int Music => MusicLoader.GetMusicSlot(Mod, "Content/Assets/Music/Boss/Wall_Of_Flesh");
     }
 }
-    
