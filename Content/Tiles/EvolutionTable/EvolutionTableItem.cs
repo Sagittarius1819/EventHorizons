30a31a0df8c4f346cbc0f4eee626a083c716af07
ORION <n/a>
2024-07-26 20:59:10 -0700

    Evolution table testing. Broken asf

---
+++ Content/Tiles/EvolutionTable/EvolutionTableItem.cs
@@ -0,0 +1,35 @@
+﻿using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace EventHorizons.Content.Tiles.EvolutionTable
+{
+    public class EvolutionTableItem : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+        }
+
+        public override void SetDefaults()
+        {
+            Item.width = 12;
+            Item.height = 12;
+            Item.maxStack = 999;
+            Item.value = 100;
+            Item.useTurn = true;
+            Item.autoReuse = true;
+            Item.useAnimation = 15;
+            Item.useTime = 10;
+            Item.useStyle = ItemUseStyleID.Swing;
+            Item.consumable = true;
+            Item.createTile = ModContent.TileType<EvolutionTableS1>();
+        }
+
+        public override void AddRecipes()
+        {
+            CreateRecipe()
+                .AddIngredient(ItemID.DirtBlock, 10)
+                .AddTile(TileID.WorkBenches)
+                .Register();
+        }
+    }
+}
