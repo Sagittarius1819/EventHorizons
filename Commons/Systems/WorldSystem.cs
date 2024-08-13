e416413b62667d8c8b16246243aaa4f9a6ed6ad0
MittyJ <nickjerden07@gmail.com>
2024-08-12 22:02:12 -0400

    Worked on the central oval created for the biome, not quite finished

--- Commons/Systems/WorldSystem.cs
+++ Commons/Systems/WorldSystem.cs
@@ -2,13 +2,83 @@
 using System.Collections.Generic;
 using EventHorizons.Commons.Systems.Genpasses;
 using Terraria.ModLoader;
+using Terraria;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+using Terraria.ID;
+using EventHorizons.Content.Tiles;
+using Steamworks;
 
 namespace TutorialMod.Common.Systems
 {
     internal class WorldSystem : ModSystem
     {
+        public static bool JustPressed(Keys key) {
+			return Main.keyState.IsKeyDown(key) && !Main.oldKeyState.IsKeyDown(key);
+		}
+
+		public override void PostUpdateWorld() {
+			if (JustPressed(Keys.D1))
+				TestMethod((int)Main.MouseWorld.X / 16, (int)Main.MouseWorld.Y / 16);
+		}
+
+		private void TestMethod(int x, int y) {
+			Dust.QuickBox(new Vector2(x, y) * 16, new Vector2(x + 1, y + 1) * 16, 2, Color.YellowGreen, null);
+
+            // Code to test placed here:
+            CreateCentralOval(x, y, 100, 30, 2);
+
+
+        }
+
+        private void CreateCentralOval(int x, int y, int height, int width, int slope)
+        {
+            //Method Not fully working
+            int center = x + width / 2;
+            for (int i = 0; i < width; i += slope) {
+                for (int j = 0; j < i; j++)
+                {
+                    for (int k = 0; k < slope; k++)
+                    {
+                        WorldGen.KillTile(center + j, y + i + k);
+                        WorldGen.KillTile(center - j, y + i + k);
+                        WorldGen.PlaceTile(center + j, y + i + k, ModContent.TileType<CrystallineStoneTile>());
+                        WorldGen.PlaceTile(center - j, y + i + k, ModContent.TileType<CrystallineStoneTile>());
+                    }
+                    
+                }
+                
+            }
+
+            for (int i = width; i < height - width; i++)
+            {
+                for (int j = 0; j < width; j++)
+                {
+                    WorldGen.KillTile(x + j, y + i);
+                    WorldGen.PlaceTile(x + j, y + i, ModContent.TileType<CrystallineStoneTile>());
+                }
+            }
+
+            for (int i = 0; i < width; i += slope)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    for (int k = 0; k < slope; k++)
+                    {
+                        WorldGen.KillTile(center + j, y + height - i + k);
+                        WorldGen.KillTile(center - j, y + height - i + k);
+                        WorldGen.PlaceTile(center + j, y + height - i + k, ModContent.TileType<CrystallineStoneTile>());
+                        WorldGen.PlaceTile(center - j, y + height - i + k, ModContent.TileType<CrystallineStoneTile>());
+                    }
+
+                }
+
+            }
+        }
         public override void ModifyWorldGenTasks(List<GenPass> tasks, ref double totalWeight)
         {
+
+            
             int shiniesIndex = tasks.FindIndex(t => t.Name.Equals("Shinies"));
             if (shiniesIndex != -1)
             {
