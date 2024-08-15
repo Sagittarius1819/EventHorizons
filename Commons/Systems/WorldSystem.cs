c8286446f68463103c0d339dfe4c2f0eb65c8ec9
MittyJ <nickjerden07@gmail.com>
2024-08-15 00:00:57 -0400

    Testing with spikes, unsuccesful

--- Commons/Systems/WorldSystem.cs
+++ Commons/Systems/WorldSystem.cs
@@ -33,7 +33,8 @@ namespace TutorialMod.Common.Systems
 
             //Tunnel placed in middle
             //CreateEllipse(x, y, 45, 30, 2, ModContent.TileType<CrystallineStoneTile>());
-            GenerateSpike(x, y, 20, -1f, -1f, ModContent.TileType<CrystallineStoneTile>());
+            CreateSpikesAroundEllipse(x, y, 40, 50, 4f, 20, 30, ModContent.TileType<CrystallineStoneTile>());
+            //GenerateSpike(x, y, 30, -.7f, -.3f, ModContent.TileType<CrystallineStoneTile>());
 
         }
 
@@ -44,6 +45,12 @@ namespace TutorialMod.Common.Systems
 
         private void GenerateSpike(int x, int y, int length, float slope, float downSlope, int type)
         {
+            //Always spawns on the left most block, spikes on the left side will need the difference added
+            //left spikes look best with a small upslope and large down slope(When speaking about the absolute value of the slope)
+            //Be very careful using a larger upslope, does not generate correctly(This applies to the absolute value)
+            if (Math.Abs(slope) > Math.Abs(downSlope)) {
+                throw new Exception("The absolute value of the downslope may not be greater than the upslope");
+            }
             int helfLength = length / 2;
             if (slope > 0)
             {
@@ -96,7 +103,6 @@ namespace TutorialMod.Common.Systems
             for (int i = 0; i < horizontalRadius; i++)
             {
                 int topY = (int)Math.Sqrt(Math.Pow(verticalRadius, 2) - ((Math.Pow(i, 2) * Math.Pow(verticalRadius, 2)) / Math.Pow(horizontalRadius, 2)));
-                Main.NewText("x: " + i + ", y:" + topY);
                 for (int j = 0; j < (topY + y) - (y - topY); j++)
                 {
                     if (Main.tile[i + x, topY + y - j].TileType == TileID.Stone)
@@ -131,6 +137,49 @@ namespace TutorialMod.Common.Systems
 
 
             }
+
+        }
+
+        private void CreateSpikesAroundEllipse(int x, int y, int verticalRadius, int horizontalRadius, float estimatedAmount, int minLength, int MaxLength, int type)
+        {
+            //GenerateSpike(x, y + verticalRadius, WorldGen.genRand.Next(minLength, MaxLength), 2, 4, type);
+            //GenerateSpike(x, y - verticalRadius, WorldGen.genRand.Next(minLength, MaxLength), -2, -4, type);
+
+            //GenerateSpike(x + horizontalRadius / 2, y + verticalRadius, WorldGen.genRand.Next(minLength * 4, MaxLength * 4), .5f, 4, type);
+            //GenerateSpike(x + horizontalRadius / 2, y - verticalRadius, WorldGen.genRand.Next(minLength * 4, MaxLength * 4), -.5f, -4, type);
+            for (int i = 0; i < horizontalRadius; i++)
+            {
+                int topY = (int)Math.Sqrt(Math.Pow(verticalRadius, 2) - ((Math.Pow(i, 2) * Math.Pow(verticalRadius, 2)) / Math.Pow(horizontalRadius, 2)));
+                WorldGen.KillTile(i + x, topY + y);
+                WorldGen.PlaceTile(i + x, topY + y, type);
+
+                WorldGen.KillTile(i + x, y - topY);
+                WorldGen.PlaceTile(i + x, y - topY, type);
+
+                WorldGen.KillTile(x - i, topY + y);
+                WorldGen.PlaceTile(x - i, topY + y, type);
+
+                WorldGen.KillTile(x - i, y - topY);
+                WorldGen.PlaceTile(x - i, y - topY, type);
+
+            }
+            //float chance = estimatedAmount / (float) horizontalRadius;
+            //for (int i = 0; i < horizontalRadius; i++)
+            //{
+            //    int topY = (int)Math.Sqrt(Math.Pow(verticalRadius, 2) - ((Math.Pow(i, 2) * Math.Pow(verticalRadius, 2)) / Math.Pow(horizontalRadius, 2)));
+            //    for (int j = 0; j < (topY + y) - (y - topY); j++)
+            //    {
+
+            //        if (WorldGen.genRand.NextFloat() < chance)
+            //        {
+
+            //            GenerateSpike(x + i, y + topY, WorldGen.genRand.Next(minLength, MaxLength), WorldGen.genRand.Next(1, 5), WorldGen.genRand.Next(5, 10), type);
+            //        }
+
+            //    }
+
+            //}
+
 
         }
 
